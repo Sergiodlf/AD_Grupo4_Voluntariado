c2f665363b9f5df38387504d2f3c50bbeaef4754
Leyre Romero Velasco <leyre.romero@gmail.com>
2026-02-13 10:52:33 +0100

    (feat)Formulario Menu y organizaciones hechos. Inicio de v2.

---
+++ CapaPresentacion/Menu.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace CapaPresentacion
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.funcionalidadesDeAnálisisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filtrarPorODSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filtrarPorVoluntariosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.organizacionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.actividadesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.funcionalidadesDeAnálisisToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1208, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // funcionalidadesDeAnálisisToolStripMenuItem
+            // 
+            this.funcionalidadesDeAnálisisToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.filtrarPorODSToolStripMenuItem,
+            this.filtrarPorVoluntariosToolStripMenuItem,
+            this.organizacionesToolStripMenuItem,
+            this.actividadesToolStripMenuItem});
+            this.funcionalidadesDeAnálisisToolStripMenuItem.Name = "funcionalidadesDeAnálisisToolStripMenuItem";
+            this.funcionalidadesDeAnálisisToolStripMenuItem.Size = new System.Drawing.Size(207, 24);
+            this.funcionalidadesDeAnálisisToolStripMenuItem.Text = "Funcionalidades De Análisis";
+            // 
+            // filtrarPorODSToolStripMenuItem
+            // 
+            this.filtrarPorODSToolStripMenuItem.Name = "filtrarPorODSToolStripMenuItem";
+            this.filtrarPorODSToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.filtrarPorODSToolStripMenuItem.Text = "Filtrar por ODS";
+            // 
+            // filtrarPorVoluntariosToolStripMenuItem
+            // 
+            this.filtrarPorVoluntariosToolStripMenuItem.Name = "filtrarPorVoluntariosToolStripMenuItem";
+            this.filtrarPorVoluntariosToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.filtrarPorVoluntariosToolStripMenuItem.Text = "Voluntarios";
+            // 
+            // organizacionesToolStripMenuItem
+            // 
+            this.organizacionesToolStripMenuItem.Name = "organizacionesToolStripMenuItem";
+            this.organizacionesToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.organizacionesToolStripMenuItem.Text = "Organizaciones";
+            // 
+            // actividadesToolStripMenuItem
+            // 
+            this.actividadesToolStripMenuItem.Name = "actividadesToolStripMenuItem";
+            this.actividadesToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.actividadesToolStripMenuItem.Text = "Actividades";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(164, 238);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(845, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bienvenid@ al panel de análisis del voluntariado4V";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(278, 305);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(585, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Empiece seleccionando una opción del menú de funcionalidades";
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1208, 625);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem funcionalidadesDeAnálisisToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem filtrarPorODSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem filtrarPorVoluntariosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem organizacionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem actividadesToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ CapaPresentacion/Organizaciones.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace CapaPresentacion
+{
+    partial class Organizaciones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvOrganizacion = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombreOrg = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrganizacion)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvOrganizacion
+            // 
+            this.dgvOrganizacion.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOrganizacion.Location = new System.Drawing.Point(120, 245);
+            this.dgvOrganizacion.Name = "dgvOrganizacion";
+            this.dgvOrganizacion.RowHeadersWidth = 51;
+            this.dgvOrganizacion.RowTemplate.Height = 24;
+            this.dgvOrganizacion.Size = new System.Drawing.Size(975, 362);
+            this.dgvOrganizacion.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(341, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre Organización:";
+            // 
+            // txtNombreOrg
+            // 
+            this.txtNombreOrg.Location = new System.Drawing.Point(500, 145);
+            this.txtNombreOrg.Name = "txtNombreOrg";
+            this.txtNombreOrg.Size = new System.Drawing.Size(382, 22);
+            this.txtNombreOrg.TabIndex = 2;
+            // 
+            // Organizaciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1231, 688);
+            this.Controls.Add(this.txtNombreOrg);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvOrganizacion);
+            this.Name = "Organizaciones";
+            this.Text = "Organizaciones";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrganizacion)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvOrganizacion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombreOrg;
+    }
+}
